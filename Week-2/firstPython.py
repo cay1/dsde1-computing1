231bee926d476b2c492e9e10d943891288cc1320
cay1 <michaelabarker19@imperial.ac.uk>
2019-10-15 14:24:27 +0100

    test

---
+++ Week-2/firstPython.py
@@ -0,0 +1,17 @@
+#message = 'If this has printed, you have run the code successfully.'
+#print(message)
+
+answer=input("enter a message: ")
+print (answer)
+num= int(input("enter a number between 1 and 10: "))
+import random
+x= random.randint(1,10)
+if num>10 or num<1:
+    print("invalid try again.")
+    
+elif x==num:
+    print('TRUE')
+else:
+    print('FALSE')
+
+
