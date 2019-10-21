e213a22c572e0b6f5e2b57d213aa7a7a8eb2c4cc
cay1 <michaelabarker19@imperial.ac.uk>
2019-10-21 12:08:05 +0100

    pendulum fin

--- Week-3/pendulumtest/pendulum.py
+++ Week-3/pendulumtest/pendulum.py
@@ -1,27 +1,23 @@
-L=int(raw_input('Enter the length of the pendulum: '))
-g=float(raw_input('Enter the accn due to gravity: '))
+import math
 
+L=float(raw_input('Enter the length of the pendulum: '))
+g=float(raw_input('Enter the accn due to gravity: ')
 
 def period(L,g):
-    isinstance(L,int)
-    print (isinstance (L, int))
-    if isinstance (L, int)==False:
-        print ('you have a Type error. Make sure the number you inputed is an integer')
-    isinstance(g,int)
-    print (isinstance (g, float))
+    
+    isinstance(L,float)
+    if isinstance (L, float)==False:
+        print ('you have a Type error. Make sure the number you inputed is a floating point number')
+    isinstance(g,float)
     if isinstance (g, float)==False:
-        print ('you have a Type error. Make sure the number you inputed is a float')
-    
-    
-    if isinstance (L, int)== True and isinstance (g, float)==True:
-        import math
+        print ('you have a Type error. Make sure the number you inputed is a floating point number')
+    if isinstance (L, float)==True and isinstance (g, float)==True:
         W= math.sqrt(L/g)
         T=2*math.pi*W
         return T
     else:
-        print ('The sum could not be computed due to type errors')
-
-
+       print ('The sum could not be computed due to type errors')
 
 print (period(L,g))
+
 
